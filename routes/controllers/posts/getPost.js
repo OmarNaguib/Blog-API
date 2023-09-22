27d5eb97d5ad39abd58467303d9e257b11aac3ff
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-23 02:36:34 +0300

    get posts

--- routes/controllers/posts/getPost.js
+++ routes/controllers/posts/getPost.js
@@ -1,7 +1,14 @@
 const asyncHandler = require("express-async-handler");
+const Post = require("../../../models/Post");
 
-const rename = asyncHandler(async (req, res, next) => {
-  res.send("respond with a resource");
+const getPost = asyncHandler(async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.postId).exec();
+    res.status(200);
+    res.json({ post });
+  } catch (error) {
+    res.sendStatus(400);
+  }
 });
 
-module.exports = rename;
+module.exports = getPost;
--- routes/controllers/posts/getPosts.js
+++ routes/controllers/posts/getPosts.js
@@ -1,7 +1,14 @@
 const asyncHandler = require("express-async-handler");
+const Post = require("../../../models/Post");
 
-const rename = asyncHandler(async (req, res, next) => {
-  res.send("respond with a resource");
+const getPosts = asyncHandler(async (req, res, next) => {
+  try {
+    const posts = await Post.find().exec();
+    res.status(200);
+    res.json({ posts });
+  } catch (error) {
+    res.sendStatus(400);
+  }
 });
 
-module.exports = rename;
+module.exports = getPosts;
