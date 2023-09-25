8f9a605e1ed6949b1da1330883888d161738c09a
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-25 11:45:11 +0300

    Allow specific requests

--- routes/controllers/posts/getPosts.js
+++ routes/controllers/posts/getPosts.js
@@ -3,7 +3,7 @@ const Post = require("../../../models/Post");
 
 const getPosts = asyncHandler(async (req, res, next) => {
   try {
-    const posts = await Post.find().exec();
+    const posts = await Post.find(req.query).exec();
     res.status(200);
     res.json({ posts });
   } catch (error) {
