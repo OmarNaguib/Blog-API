b07882df154c04dce4c23f353531fede74257657
omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-27 01:37:19 -0400

    Delete comments with post

--- routes/controllers/comments/getComments.js
+++ routes/controllers/comments/getComments.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Comment = require("../../../models/Comment");
 
 const getComments = asyncHandler(async (req, res, next) => {
-  const comments = await Comment.find().exec();
+  const comments = await Comment.find({ post: req.params.postId }).exec();
   res.status(200);
   res.json({ comments });
 });
--- routes/controllers/posts/deletePost.js
+++ routes/controllers/posts/deletePost.js
@@ -1,8 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const Post = require("../../../models/Post");
+const Comment = require("../../../models/Comment");
 
 const deletePost = asyncHandler(async (req, res, next) => {
   await Post.findByIdAndDelete(req.params.postId);
+  const result = await Comment.deleteMany({ post: req.params.postId });
+  console.log(result);
   res.sendStatus(200);
 });
 
