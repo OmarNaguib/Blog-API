ddba4735e80db94c7db13f21ebda18548a93160b
omar <55961349+OmarNaguib@users.noreply.github.com>
2023-10-01 06:51:13 +0300

    Get tokens

--- routes/controllers/login.js
+++ routes/controllers/login.js
@@ -1,16 +1,26 @@
 const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
 require("dotenv").config();
 
-const login = (req, res, next) => {
+const login = asyncHandler((req, res, next) => {
   if (req.body.password === process.env.PASSWORD)
     jwt.sign(
       { user: "admin" },
       process.env.SECRET_KEY,
-      { expiresIn: "30s" },
+      { expiresIn: 30 },
       (err, token) => {
-        res.json({ token, expirationTime: "30s" });
+        res.json({ token, expirationTime: 30 });
       }
     );
   else res.sendStatus(401);
-};
-module.exports = login;
+});
+
+const getToken = asyncHandler((req, res, next) => {
+  const bearerHeader = req.headers.authorization;
+  if (bearerHeader === undefined) res.sendStatus(403);
+  const token = bearerHeader.split(" ")[1];
+  req.token = token;
+  next();
+});
+module.exports = { login, getToken };
